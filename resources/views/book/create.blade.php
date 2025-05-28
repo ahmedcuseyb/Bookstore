e2b526c007331f259228917e2859e2e38ca76022
Ahmed Bicir <ahmedhassanbicir.aa98@example.com>
2025-05-28 10:09:44 +0300

    fixed error

--- resources/views/book/create.blade.php
+++ resources/views/book/create.blade.php
@@ -21,7 +21,7 @@
     <div class="bg-white shadow-md rounded-xl p-6">
         <div class="flex justify-between items-center mb-6">
             <h2 class="text-2xl font-semibold">Add Categories</h2>
-            <a href="{{ url('categories') }}" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md text-sm font-medium">
+            <a href="{{ url('books') }}" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md text-sm font-medium">
                 Back
             </a>
         </div>
